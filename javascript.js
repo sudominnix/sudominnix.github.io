ed880b87cc310cc585da8c958e157ce76311d7c3
Matthew Minnix <sudominnix@gmail.com>
2014-10-03 04:55:46 -0500

    double push

--- javascript.js
+++ javascript.js
@@ -1,39 +1,68 @@
+// PURELY JQUERY
+
+$(document).ready(function() {
+  	$( "#loaded" ).text( "The DOM is now loaded and can be manipulated." );
+  	});
+
+$(document).ready(function(){
+	alert("Hello World");
+	});
+
+
+$( "#foo" ).bind( "click", function() {
+  alert( "User clicked on 'foo.'" );
+});
+
+$( "#fu" ).click(function() {
+	console.log("yes it worked");
+});
 
 
 
-// CODE FOR repeating a entered line a specified (by user) number of times.
 
+// CODE FOR TEMPERATURE CONVERSION
 
-
-
-
-
-function slogan() {
-    var newslogan = prompt("what you say?");
-    var numbertimes = prompt("how many you want?");
-   for (var i = 0; i < (numbertimes); i++) {
-    var node=document.createElement("P");
-    var textnode=document.createTextNode(newslogan)
-    node.appendChild(textnode);
-    document.getElementById("sloganline").appendChild(node);
-  }
+function ftoc() {
+    var tf = document.getElementById('itemp').value;
+    var tc = (tf-32)*(5/9);
+    var result = Math.round(tc*100)/100;
+    document.getElementById('rtemp').innerHTML = result+"C";
 }
 
-function switchfunction() {
-    var answer = prompt("what is your favorite color- red, blue or white?").toLowerCase();
-    switch(answer){
-        case 'red':
-        var useranswer="Red is nice."
-        break;
-        case 'blue':
-        var useranswer="Blue is nice."
-        break;
-        case 'white':
-        var useranswer="White is really nice."
-        break;
-        default:
-        var useranswer="Sorry, it has to be red, white or blue.";
-    }
-    document.getElementById('switchline').innerHTML = useranswer;
+function ctof() {
+	var tc = document.getElementById('itemp').value;
+	var tf = 32+(tc*(9/5));
+	var result = Math.round(tf*100)/100;
+	document.getElementById('rtemp').innerHTML = result+"F";
+}
 
-};
+
+// CODE FOR TEMP CONVERSIONS
+
+function swapconversion() {
+	$("#changectofbutton").toggle();
+	$("#changeftocbutton").toggle();
+	$("#ctof").toggle();
+	$("#ftoc").toggle();
+	document.getElementById('rtemp').innerHTML = "";
+	document.getElementById('itemp').value = "";
+
+}
+
+// EXPERIMENT FROM GITHUB -> plugin
+// https://github.com/fkling/jQuery-Function-Toggle-Plugin
+
+$(".multicolorfun").funcToggle({
+    'click': [function() {
+            $(this).css('color', 'red');
+        }, function() {
+            $(this).css('color', 'green');
+        }, function() {
+            $(this).css('color', 'black');
+        }],
+    'mouseover': [function() {
+            $(this).css('background-color', 'red');
+        }, function() {
+             $(this).css('background-color', 'white');
+        }]
+});
